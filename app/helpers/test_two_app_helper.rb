c003da1a7ddd482ea46439b47a77489eb342f5c9
NeilParker1 <neil.parker@f3mmedia.com>
2015-01-04 14:50:03 +0000

    change static html to rails tag helpers, move html into view helper

--- app/helpers/test_two_app_helper.rb
+++ app/helpers/test_two_app_helper.rb
@@ -1,2 +1,22 @@
 module TestTwoAppHelper
+
+  def get_predict_match_html(match)
+    content = content_tag(:div,
+                get_predict_team_item(match[:home_team][:name]),
+                class: 'predictTeam')
+    content + content_tag(:div,
+                get_predict_team_item(match[:away_team][:name]),
+                class: 'predictTeam')
+  end
+
+  def get_predict_team_item(team_name)
+    content = content_tag(:div,
+                team_name,
+                class: 'predictTeamItem teamName')
+    content + content_tag(:div,
+                text_field_tag(team_name.downcase.gsub(' ', '_').to_sym, nil, class: 'inputBox'),
+                class: 'predictTeamItem')
+  end
+
 end
+
