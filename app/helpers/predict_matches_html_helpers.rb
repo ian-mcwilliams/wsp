40433618a65edff1b5112850c069f8d1ee61ccb6
NeilParker1 <neil.parker@f3mmedia.com>
2015-02-05 20:57:25 +0000

    add list and list detail methods to predict_matches_html_helpers.rb

--- app/helpers/predict_matches_html_helpers.rb
+++ app/helpers/predict_matches_html_helpers.rb
@@ -3,6 +3,47 @@ module PredictMatchesHtmlHelpers
   # =============================   PREDICT MATCHES FORM   ==============================
   # =====================================================================================
 
+  def set_make_predictions_page_html_list
+    @html_list = {
+        predict_match_form_div: {
+            predict_match_form: {
+                predict_match_div: {
+                    predict_match_text: {},
+                    predict_match_input: {}
+                },
+                predict_match_form_submit: {}
+            }
+        }
+    }
+  end
+
+  def set_make_predictions_page_html_list_detail
+    @html_list_detail = {
+        predict_match_form_div: {
+            tag: :div
+        },
+        predict_match_form: {
+            tag: :form
+        },
+        predict_match_div: {
+            tag: :div,
+            loop: {
+                each: @match
+            }
+        },
+        predict_match_text: {
+            tag: :text
+        },
+        predict_match_input: {
+            tag: :input
+        },
+        predict_match_form_submit: {
+            tag: :submit
+        }
+    }
+  end
+
+
   def get_make_predictions_page_html
     @current_page_html_content_str << content_tag(:div,
                                                   get_make_predictions_form_html,
