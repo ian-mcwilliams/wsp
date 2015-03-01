27686823f80574b3e495ff04bcaa3770fdbbe743
NeilParker1 <neil.parker@f3mmedia.com>
2015-03-01 13:22:06 +0000

    remove redundant methods

--- app/helpers/login_html_helpers.rb
+++ app/helpers/login_html_helpers.rb
@@ -95,32 +95,4 @@ module LoginHtmlHelpers
     }
   end
 
-
-
-
-
-
-
-  def get_login_page_htmlx
-    @current_page_html_content_str << content_tag(:div,
-                                                  get_login_form_html,
-                                                  id: 'loginForm')
-  end
-
-  def get_login_form_html
-    form_tag(controller: 'test_two_app', action: 'login', method: 'post') do
-      get_login_form_content_html
-    end
-  end
-
-  def get_login_form_content_html
-    content = content_tag(:div,
-                          get_form_input_html('Email', email_field(:user, :email)),
-                          class: 'formElement')
-    content << content_tag(:div,
-                           get_form_input_html('Password', password_field_tag(:password)),
-                           class: 'formElement')
-    content + content_tag(:div, submit_tag('Login'), class: 'submitDiv')
-  end
-
 end
