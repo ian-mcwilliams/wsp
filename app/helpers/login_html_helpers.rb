f623ea3dc3c3ecde3f39ff87b09bfdeed62d3558
NeilParker1 <neil.parker@f3mmedia.com>
2015-03-07 14:40:09 +0000

    restructure login page

--- app/helpers/login_html_helpers.rb
+++ app/helpers/login_html_helpers.rb
@@ -7,22 +7,16 @@ module LoginHtmlHelpers
   def get_login_page_html_list
     {
         left_margin_div: {},
-        login_form_div: {
-            login_form: {
-                login_email_div: {
-                    login_email_text: {},
-                    login_email_input_div: {
-                        login_email_input: {}
-                    }
-                },
-                login_password_div: {
-                    login_password_text_div: {},
-                    login_password_input_div: {
-                        login_password_input: {}
-                    }
-                },
-                login_form_submit: {}
-            }
+        login_form: {
+            login_email_div: {
+                login_email_text: {},
+                login_email_input: {}
+            },
+            login_password_div: {
+                login_password_text: {},
+                login_password_input: {}
+            },
+            login_form_submit: {}
         },
         right_margin_div: {}
     }
@@ -35,13 +29,10 @@ module LoginHtmlHelpers
             tag: :div,
             args: { class: 'col-xs-4'}
         },
-        login_form_div: {
-            tag: :div,
-            args: { class: 'col-xs-4' }
-        },
         login_form: {
             tag: :form,
             args: { controllor: 'test_two_app', action: 'login', method: 'post' },
+            div: { args: { class: 'col-xs-4' } }
         },
         login_email_div: {
             tag: :div,
@@ -49,42 +40,31 @@ module LoginHtmlHelpers
         },
         login_email_text: {
             text: 'Email:',
-            div: {
-                args: { class: 'col-xs-6' }
-            }
-        },
-        login_email_input_div: {
-            tag: :div,
-            args: { class: 'col-xs-6' }
+            div: { args: { class: 'col-xs-6' } }
         },
         login_email_input: {
             tag: :input,
-            args: { class: '' }
+            args: { class: '' },
+            div: { args: { class: 'col-xs-6' } }
         },
         login_password_div: {
             tag: :div,
             args: { class: 'row' }
         },
-        login_password_text_div: {
-            tag: :div,
-            args: { class: 'col-xs-6' },
-            text: 'Password:'
-        },
-        login_password_input_div: {
-            tag: :div,
-            args: { class: 'col-xs-6' }
+        login_password_text: {
+            text: 'Password:',
+            div: { args: { class: 'col-xs-6' } }
         },
         login_password_input: {
             tag: :input,
-            args: { class: '' }
+            args: { class: '' },
+            div: { args: { class: 'col-xs-6' } }
         },
         login_form_submit: {
             tag: :submit,
             args: { class: 'btn-primary btn-lg' },
             text: 'Login',
-            div: {
-                args: { class: 'row' }
-            }
+            div: { args: { class: 'row' } }
         },
         right_margin_div: {
             tag: :div,
