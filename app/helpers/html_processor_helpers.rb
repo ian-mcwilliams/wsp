76f27d4e3ab0b74fb7148fad7ee31b04cfbe0b84
NeilParker1 <neil.parker@f3mmedia.com>
2015-03-07 20:00:33 +0000

    fix html processor updates to work in loops

--- app/helpers/html_processor_helpers.rb
+++ app/helpers/html_processor_helpers.rb
@@ -15,39 +15,50 @@ module HtmlProcessorHelpers
 
   def execute_html_processing(html_list, loop_id=nil)
     current_str = active_support_str
-    html_list.each do |key, value|
-      puts 'DEBUG'
-      puts "key = #{key}"
-      @detail = @html_list_detail[key]
-      pp ">>>#{@detail}"
-      set_text_div_detail if @detail.has_key?(:div) && @detail.has_key?(:text) && !@detail.has_key?(:tag)
-      if div_wrap_tags.include?(@detail[:tag]) && @detail.has_key?(:div)
-        div_detail = @detail.delete(:div)
-        @html_list_detail[key] = @detail
-        set_div_wrap_element_detail(div_detail)
-        descendant_html = {}
-        descendant_html[key] = value
-        raw_id = "#{key.to_s}_div".to_sym
-      else
-        descendant_html = value
-        raw_id = key
-      end
-      puts '/DEBUG'
-      loop_id ? id = "#{loop_id}#{snake_to_camel(raw_id.to_s, true)}" : id = snake_to_camel(raw_id.to_s)
-      if @detail.has_key?(:loop)
-        @iterate ? top_level_iterator = false : top_level_iterator = true
-        @iterate = true if top_level_iterator
-        collection = get_collection
-        collection[:items].each_with_index do |item, index|
-          current_id = "#{id}#{index}"
-          instance_variable_set(collection[:subset], item) if collection.has_key?(:subset)
-          current_str << build_html_str(current_id, descendant_html)
-        end
-        @iterate = false if top_level_iterator
-      else
-        current_str << build_html_str(id, descendant_html)
-      end
+    html_list.each { |key, value| current_str << process_html_element(key, value, loop_id) }
+    current_str
+  end
+
+  def process_html_element(key, value, loop_id)
+    @detail = {}.merge(@html_list_detail[key])
+    if @div_wrapper
+      puts '2' if key == :predict_match_team_input
+      @detail.delete(:div)
+      @div_wrapper = false
     end
+    set_text_div_detail if @detail.has_key?(:div) && @detail.has_key?(:text) && !@detail.has_key?(:tag)
+    if div_wrap_tags.include?(@detail[:tag]) && @detail.has_key?(:div)
+      @div_wrapper = true
+      div_detail = @detail.delete(:div)
+      set_div_wrap_element_detail(div_detail)
+      descendant_html = {}
+      descendant_html[key] = value
+      raw_id = "#{key.to_s}_div".to_sym
+    else
+      descendant_html = value
+      raw_id = key
+    end
+    loop_id ? html_id = "#{loop_id}#{snake_to_camel(raw_id.to_s, true)}" : html_id = snake_to_camel(raw_id.to_s)
+    if @detail.has_key?(:loop)
+      current_str = process_loop(html_id, descendant_html)
+    else
+      current_str = active_support_str
+      current_str << build_html_str(html_id, descendant_html)
+    end
+    current_str
+  end
+
+  def process_loop(html_id, descendant_html)
+    @iterate ? top_level_iterator = false : top_level_iterator = true
+    @iterate = true if top_level_iterator
+    collection = get_collection
+    current_str = active_support_str
+    collection[:items].each_with_index do |item, index|
+      current_id = "#{html_id}#{index}"
+      instance_variable_set(collection[:subset], item) if collection.has_key?(:subset)
+      current_str << build_html_str(current_id, descendant_html)
+    end
+    @iterate = false if top_level_iterator
     current_str
   end
 
@@ -64,16 +75,16 @@ module HtmlProcessorHelpers
     }.merge(div_detail)
   end
 
-  def build_html_str(id, value)
+  def build_html_str(html_id, value)
     @args = nil
-    id_hash = { id: id }
+    id_hash = { id: html_id }
     @detail.has_key?(:args) ? @args = @detail[:args].merge(id_hash) : @args = id_hash
     process_text
     args, detail = {}, {}
     args.merge!(@args); detail.merge!(@detail)
     current_str = nil
     unless value.empty?
-      @iterate ? current_str = execute_html_processing(value, id) : current_str = execute_html_processing(value)
+      @iterate ? current_str = execute_html_processing(value, html_id) : current_str = execute_html_processing(value)
     end
     @args = args; @detail = detail
     @args[:content] = current_str if current_str
